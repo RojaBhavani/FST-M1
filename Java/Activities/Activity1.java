017f24c938c710b690ae972837568946377fd247
RojaBhavani <lalaji.roja.bhavani.sadhanand@ibm.com>
2026-06-12 09:02:42 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,16 @@
+package activities;
+
+public class Activity1 
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		Car carname = new Car("black","Automatic",2025,4,4);
+		carname.displayCharacteristics();
+		carname.accelerate();
+		carname.brake();
+
+	}
+
+}
