7305b76106e1092eb1e80755e1f9468b672752cf
RojaBhavani <lalaji.roja.bhavani.sadhanand@ibm.com>
2026-06-15 08:50:18 +0530

    Add files via upload

---
+++ Java/Activities/Activity4.java
@@ -0,0 +1,35 @@
+package activities;
+
+
+class Activity4 
+{
+
+    public static void main(String[] args) 
+    {
+        int[] arr = {45, 12, 78, 34, 23, 90, 11};
+
+        System.out.println("Array before sorting:");
+        printArray(arr);
+        for (int i = 1; i < arr.length; i++) 
+        {
+            int current = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > current) 
+            {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = current;
+        }
+        System.out.println("\nArray after sorting:");
+        printArray(arr);
+    }
+    public static void printArray(int[] arr)
+    {
+        for (int num : arr) 
+        {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
